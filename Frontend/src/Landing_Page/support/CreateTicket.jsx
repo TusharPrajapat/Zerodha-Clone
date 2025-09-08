fb0d38edebb8b6a5c444df4e2e906b98b1aa296c
TusharPrajapat <tusharprajapat41@gmail.com>
2025-09-08 12:16:00 +0530

    Set up support component

--- Frontend/src/Landing_Page/support/CreateTicket.jsx
+++ Frontend/src/Landing_Page/support/CreateTicket.jsx
@@ -1,7 +1,180 @@
 import React from "react";
 
 function CreateTicket() {
-  return <h1>CreateTicket</h1>;
+  return (
+    <div className="container mt-5">
+      <div>
+        <h2>To create a ticket, select a relevant topic</h2>
+      </div>
+      <div className="row mt-5">
+        <div className="col ">
+          <h5 className="">
+            {" "}
+            <i class="fa fa-plus-circle" aria-hidden="true"></i> Account Opening
+          </h5>
+
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Online Account Opening
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Offline Account Opening
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Company, Partnership and HUF Account Opening
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            NRI Account Opening
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Charges at Zerodha
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Zerodha IDFC FIRST Bank 3-in-1 Account
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Getting Started
+          </a>
+        </div>
+        <div className="col">
+          <h5>
+            <i class="fa fa-user" aria-hidden="true"></i> Your Zerodha Account
+          </h5>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Login Credentials
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Account Modification and Segment Addition
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            DP ID and bank details
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Your Profile
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Transfer and conversion of shares
+          </a>
+        </div>
+        <div className="col">
+          <h5>
+            <i class="fa fa-bar-chart" aria-hidden="true"></i> Trading and
+            Markets
+          </h5>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Margin/leverage, Product and Order types
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Kite Web and Mobile
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Trading FAQs
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Corporate Actions
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Sentinel
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Kite API
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Pi and other platform
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            Stockreports+
+          </a>
+          <a
+            href=""
+            style={{ textDecoration: "none" }}
+            className="d-block mt-4"
+          >
+            GTT
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- Frontend/src/Landing_Page/support/Hero.jsx
+++ Frontend/src/Landing_Page/support/Hero.jsx
@@ -1,7 +1,58 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-    return ( <h1>Hero</h1> );
+  return (
+    <div className="container-fluid" id="supportHero">
+      <div className="p-5" id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="" style={{ color: "white" }}>
+          Track Tickets
+        </a>
+      </div>
+      <div className="row  " id="supportWrapper">
+        <div className="col-7 p-5 ">
+          <h3 className="fs-3 mb-4">
+            Search for an answer or browse help topics to create a ticket
+          </h3>
+          <input
+            className="mb-4"
+            type="text"
+            placeholder="Eg: how do i activate F&O, why is my order getting rejected.."
+          />
+          <br></br>
+          <a href="" style={{ marginRight: "15px" }}>
+            Track Account Opening
+          </a>
+          <a href="" style={{ marginRight: "15px" }}>
+            Track Segment Activation
+          </a>
+          <a href="" style={{ marginRight: "15px" }}>
+            Intraday
+          </a>
+          <a href="" style={{ marginRight: "15px" }}>
+            Margins
+          </a>
+          <br></br>
+          <a href="" style={{ marginRight: "15px" }}>
+            Kite user manual
+          </a>
+        </div>
+        <div className="col p-5" style={{ marginLeft: "" }}>
+          <h3 className="fs-3">Featured</h3>
+          <ol>
+            <li>
+              <a href="" className="d-block mb-2">
+                Current TakeOve and Delistings
+              </a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
