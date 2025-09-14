15708148e1c4e509736e5cbc0d86dd3cfa3d4c39
TusharPrajapat <tusharprajapat41@gmail.com>
2025-09-14 19:46:00 +0530

    setup orders component

--- Dashboard/src/Components/Orders.jsx
+++ Dashboard/src/Components/Orders.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Orders() {
-  return <h1>Orders</h1>;
+  return (
+    <div className="orders">
+      <div className="no-orders">
+        <p>You haven't placed any orders today</p>
+        <Link to={"/"} className="btn">
+          Get started
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
